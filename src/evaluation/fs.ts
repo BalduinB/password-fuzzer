0cd2317838c429fa7cfb3610e3f0bc78bd104fa5
BalduinB <bb@nessy.online>
2024-05-13 19:42:26 +0200

    async walk

--- src/evaluation/fs.ts
+++ src/evaluation/fs.ts
@@ -1,6 +1,7 @@
-import * as fs from "fs";
+import * as fs from "fs/promises";
 import * as path from "path";
 import { isEmail } from "./helpers/validator";
+import { lstatSync } from "fs";
 
 export const basePathOfBreachData = path.join(__dirname, "../CompilationOfManyBreaches/data");
 export const lineDelimiter = "\r\n";
@@ -77,30 +78,53 @@ export function parseLine(line: string) {
     return line.split(":");
 }
 
-export function readDir(path: string) {
-    return fs.readdirSync(path);
+export async function readDir(path: string) {
+    return await fs.readdir(path);
 }
 
-export function readFile(filePath: string) {
-    return fs.readFileSync(filePath, { encoding: "utf8" });
+export async function readFile(filePath: string) {
+    return await fs.readFile(filePath, { encoding: "utf8" });
 }
-export function walkWhile(
+const FILES_TO_SKIP = ".DS_Store";
+
+export async function walkWhile(
     basePath: string,
     takeNext: () => boolean,
     onIsFile: (content: string) => void,
 ) {
-    const files = readDir(basePath); // Dont check takeNext, readDir is not that heavy.
+    const content = await readDir(basePath);
+    const { mateches: files, misses: notFiles } = match(content, (file) => {
+        const filePath = path.resolve(basePath, file);
+        const fileStats = lstatSync(filePath);
+        return fileStats.isFile();
+    });
+    for (const file of notFiles) {
+        if (!takeNext()) return; // Stop walking if we want to cancel File scanning.
+        const filePath = path.resolve(basePath, file);
+        const fileStats = await fs.lstat(filePath);
+        if (fileStats.isDirectory()) {
+            await walkWhile(filePath, takeNext, onIsFile);
+        } else console.log("Unknown file type: " + filePath);
+    }
     for (const file of files) {
         if (!takeNext()) return; // Stop walking if we want to cancel File scanning.
-        const filePath = path.join(basePath, file);
-        const fileStats = fs.lstatSync(filePath);
-        if (fileStats.isFile() && file !== ".DS_Store") {
-            // console.log("READING FILE", filePath.slice(basePathOfBreachData.length + 1));
+        const filePath = path.resolve(basePath, file);
 
-            onIsFile(readFile(filePath));
-        } else if (fileStats.isDirectory()) {
-            walkWhile(filePath, takeNext, onIsFile);
-        } else console.log("Unknown file type: " + filePath);
+        if (file !== FILES_TO_SKIP) {
+            console.log("READING FILE", filePath.slice(basePathOfBreachData.length + 1));
+            const content = await readFile(filePath);
+            onIsFile(content);
+        } else console.log("DS_STORE");
     }
 }
 3_002_859_753;
+
+function match<T>(arr: Array<T>, matcher: (item: T) => boolean) {
+    const mateches: Array<T> = [];
+    const misses: Array<T> = [];
+    for (const item of arr) {
+        if (matcher(item)) mateches.push(item);
+        else misses.push(item);
+    }
+    return { mateches, misses };
+}
